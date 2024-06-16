4ad4331c603e0027c422d9b5759d290278788db5
Irina <lp.ir@yandex.ru>
2024-06-16 17:05:51 +0300

    db_4.doc

--- main.py
+++ main.py
@@ -5258,7 +5258,7 @@ import re
 # if __name__ == '__main__':
 #     main()
 
-import sqlite3
+# import sqlite3
 
 # con = sqlite3.connect("profile.db")
 # cur = con.cursor()
@@ -5268,15 +5268,251 @@ import sqlite3
 #
 # con.close()
 
-with sqlite3.connect("profile.db") as con:
-    cur = con.cursor()
+# with sqlite3.connect("profile.db") as con:
+#     cur = con.cursor()
+#
+#     cur.execute('''CREATE TABLE IF NOT EXISTS users(
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     name TEXT NOT NULL,
+#     suma REAL,
+#     date BLOB)''')
 
-    cur.execute('''CREATE TABLE IF NOT EXISTS users(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    suma REAL,
-    date BLOB)''')
+# cur.execute("DROP TABLE users")
 
-    # cur.execute("DROP TABLE users")
+# import sqlite3
+#
+# with sqlite3.connect("users.db") as con:
+#     cur = con.cursor()
+
+# cur.execute('''CREATE TABLE IF NOT EXISTS persons(
+# id INTEGER PRIMARY KEY AUTOINCREMENT,
+# name TEXT NOT NULL,
+# phone BLOB NOT NULL DEFAULT "+79090000000",
+# age INTEGER CHECK(age > 0 AND age < 100),
+# email TEXT UNIQUE
+# )''')
+
+# cur.execute("""
+# ALTER TABLE persons
+# RENAME TO persons_table;
+# """)
+
+# cur.execute("""
+# ALTER TABLE persons_table
+# ADD COLUMN address NOT NULL DEFAULT "Москва"
+# """)
+
+# cur.execute("""
+# ALTER TABLE persons_table
+# DROP COLUMN address
+# """)
+
+# cur.execute("""
+# ALTER TABLE persons_table
+# RENAME COLUMN address TO home_address
+# """)
+
+# cur.execute("""
+# DROP TABLE persons_table
+# """)
+
+# import sqlite3
+#
+# with sqlite3.connect("db_3.db") as con:
+#     cur = con.cursor()
+#
+#     cur.execute("""
+#     SELECT *
+#     FROM T1
+#     LIMIT 2, 5
+#     """)
+
+# for i in cur:
+#     print(cur)
+
+# res = cur.fetchone()
+# print(res)
+#
+# res2 = cur.fetchmany(2)
+# print(res2)
+
+# res3 = cur.fetchall()
+# print(res3)
+#
+# import sqlite3
+#
+# cars_list = [
+#     ('BMV', 54000),
+#     ('Chevrolet', 46000),
+#     ('Daewoo', 36000),
+#     ('Citroen', 29000),
+#     ('Honda', 33000)
+#
+# ]
+#
+# with sqlite3.connect("car.db") as con:
+#     cur = con.cursor()
+#     cur.execute("""
+#     CREATE TABLE IF NOT EXISTS cars(
+#         car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         model TEXT,
+#         price INTEGER
+#     )
+#     """)
+#
+#     cur.executescript("""
+#     DELETE FROM cars WHERE model LIKE 'B%';
+#     UPDATE cars SET price = price + 100;
+#     """)
+
+    # cur.executemany("INSERT INTO cars VALUES(NULL, ?, ?)", cars_list)
+
+    # for car in cars_list:
+    #     cur.execute("INSERT INTO cars VALUES(NULL, ?, ?)", car)
+
+    # cur.execute("INSERT INTO cars VALUES(1, 'Renault', 22000)")
+    # cur.execute("INSERT INTO cars VALUES(2, 'Volvo', 29000)")
+    # cur.execute("INSERT INTO cars VALUES(3, 'Mercedes', 57000)")
+    # cur.execute("INSERT INTO cars VALUES(4, 'Bentley', 35000)")
+    # cur.execute("INSERT INTO cars VALUES(5, 'Audi', 52000)")
+
+# con.commit()
+# con.close()
+
+
+# import sqlite3
+#
+# con = None
+# try:
+#     con = sqlite3.connect("car.db")
+#     cur = con.cursor()
+#     cur.executescript("""
+#     CREATE TABLE IF NOT EXISTS cars(
+#         car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         model TEXT,
+#         price INTEGER
+#     );
+#     BEGIN;
+#     INSERT INTO cars VALUES(NULL, 'Renault', 22000);
+#     UPDATE cars SET price = price + 100;
+#     """)
+#     con.commit()
+# except sqlite3.Error as e:
+#     if con:
+#         con.rollback()
+#     print("Ошибка выполнения запроса")
+# finally:
+#     if con:
+#         con.close()
+
+
+# import sqlite3
+#
+# with sqlite3.connect("car.db") as con:
+#     con.row_factory = sqlite3.Row
+#     cur = con.cursor()
+#     cur.executescript("""
+#     CREATE TABLE IF NOT EXISTS cars(
+#         car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         model TEXT,
+#         price INTEGER
+#     );
+#     CREATE TABLE IF NOT EXISTS cost(
+#         name TEXT, tr_in INTEGER, buy INTEGER
+#     );
+#     CREATE TABLE IF NOT EXISTS cost(
+#         name TEXT, tr_in INTEGER, buy INTEGER
+#     );
+#     """)
+
+    # cur.execute("INSERT INTO cars VALUES(NULL, 'Запорожец', 1000)")
+    # last_row_id = cur.lastrowid
+    # buy_car_id = 2
+    # cur.execute("INSERT INTO cost VALUES('Илья', ?, ?)", (last_row_id, buy_car_id))
+
+    # cur.execute("SELECT model, price FROM cars")
+
+    # rows = cur.fetchone()
+    # print(rows)
+    #
+    # rows1 = cur.fetchmany(5)
+    # print(rows1)
+    #
+    # rows2 = cur.fetchall()
+    # print(rows2)
+
+    # for res in cur:
+    #     # print(res[0], "=>", res[1])
+    #     print(res["model"], "=>", res["price"])
+
+# import sqlite3
+#
+#
+# def read_ava(n):
+#     try:
+#         with open(f"avatars/{n}.png", "rb") as f:
+#             return f.read()
+#     except IOError as e:
+#         print(e)
+#         return False
+#
+#
+# def write_ava(name, data):
+#     try:
+#         with open(name, "wb") as f:
+#             f.write(data)
+#     except IOError as e:
+#         print(e)
+#
+#
+# with sqlite3.connect("car.db") as con:
+#     con.row_factory = sqlite3.Row
+#     cur = con.cursor()
+#     cur.executescript("""
+#     CREATE TABLE IF NOT EXISTS users(
+#         name TEXT,
+#         ava BLOB,
+#         score INTEGER
+#     )""")
+#
+#     # img = read_ava(1)
+#     # if img:
+#     #     binary = sqlite3.Binary(img)
+#     #     cur.execute("INSERT INTO users VALUES('Илья', ?, 1000)", (binary,))
+#
+#     cur.execute("SELECT ava FROM users")
+#     img = cur.fetchone()['ava']
+#
+#     write_ava("out.png", img)
+
+import sqlite3
+
+# with sqlite3.connect("car.db") as con:
+#     # con.row_factory = sqlite3.Row
+#     cur = con.cursor()
+#
+#     with open("sql_dump.sql", "w") as f:
+#         for sql in con.iterdump():
+#             f.write(sql)
+
+# with sqlite3.connect("car_project.db") as con:
+#     cur = con.cursor()
+#
+#     with open("sql_dump.sql", "r") as f:
+#         sql = f.read()
+#         cur.executescript(sql)
+
+# ORM (Object-Relation Mapping) - реляционное сопоставление объектов
+# SQLAlchemy ORM
+
+import os
+
+from models.database import DATABASE_NAME
+import create_database as db_creator
+
+if __name__ == '__main__':
+    db_is_created = os.path.exists(DATABASE_NAME)
+    if not db_is_created:
+        db_creator.create_database()
 
 
