f9dff496b3e301b4ff0e827d0caeab0801c39bae
Irina <lp.ir@yandex.ru>
2024-07-07 11:44:56 +0300

    ten

--- two/dbsite.py
+++ two/dbsite.py
@@ -36,9 +36,34 @@ def get_db():
 def index():
     db = get_db()
     dbase = FDataBase(db)
-    return render_template('index.html', menu=dbase.get_menu())
+    return render_template('index.html', menu=dbase.get_menu(), posts=dbase.get_posts_annonce())
 
 
+@app.route("/add_post", methods=["POST", "GET"])
+def add_post():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.add_post(request.form['name'], request.form['post'])
+            if not res:
+                flash("Ошибка добавления статьи", category='error')
+            else:
+                flash("Статья добавлена успешно", category='success')
+        else:
+            flash("Ошибка добавления статьи", category='error')
+
+    return render_template('add_post.html', menu=dbase.get_menu(), title="Добавление статьи")
+
+
+@app.route("/post/<int:id_post>")
+def show_post(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+
+    title, post = dbase.get_post(id_post)
+    return render_template('post.html', menu=dbase.get_menu(), title=title, post=post)
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'link_db'):
